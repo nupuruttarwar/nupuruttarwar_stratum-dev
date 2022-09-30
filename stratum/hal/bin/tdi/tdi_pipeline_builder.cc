089be2f83de9e20671094dfa70a1a20b70f46659
Derek G Foster <derek.foster@intel.com>
2022-09-30 16:13:43 -0700

    Implement DPDK-specific TDI classes
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- stratum/hal/bin/tdi/tdi_pipeline_builder.cc
+++ stratum/hal/bin/tdi/tdi_pipeline_builder.cc
@@ -33,8 +33,8 @@ constexpr char kUsage[] =
     R"USAGE(usage: -p4c_conf_file=/path/to/bf-p4c/output/program.conf -bf_pipeline_config_binary_file=$PWD/bf-pipeline.pb.bin
 
 This program assembles a Stratum-tdi pipeline protobuf message from the output
-of the Barefoot P4 compiler. The resulting message can be pushed to Stratum in
-the p4_device_config field of the P4Runtime SetForwardingPipelineConfig message.
+of the P4 compiler. The resulting message can be pushed to Stratum in the
+p4_device_config field of the P4Runtime SetForwardingPipelineConfig message.
 )USAGE";
 
 ::util::Status Unpack() {
--- stratum/hal/lib/tdi/tdi_node.h
+++ stratum/hal/lib/tdi/tdi_node.h
@@ -13,14 +13,14 @@
 #include "p4/v1/p4runtime.pb.h"
 #include "stratum/glue/integral_types.h"
 #include "stratum/glue/status/status.h"
+#include "stratum/hal/lib/common/common.pb.h"
+#include "stratum/hal/lib/common/writer_interface.h"
 #include "stratum/hal/lib/tdi/tdi.pb.h"
 #include "stratum/hal/lib/tdi/tdi_action_profile_manager.h"
 #include "stratum/hal/lib/tdi/tdi_counter_manager.h"
 #include "stratum/hal/lib/tdi/tdi_packetio_manager.h"
 #include "stratum/hal/lib/tdi/tdi_pre_manager.h"
 #include "stratum/hal/lib/tdi/tdi_table_manager.h"
-#include "stratum/hal/lib/common/common.pb.h"
-#include "stratum/hal/lib/common/writer_interface.h"
 
 namespace stratum {
 namespace hal {
@@ -70,7 +70,9 @@ class TdiNode {
       TdiPacketioManager* tdi_packetio_manager,
       TdiPreManager* tdi_pre_manager,
       TdiCounterManager* tdi_counter_manager,
-      TdiSdeInterface* tdi_sde_interface, int device_id);
+      TdiSdeInterface* tdi_sde_interface, int device_id,
+      // Note: bfrt_node defaults are (true, 1)
+      bool initialized = false, uint64 node_id = 0);
 
   // TdiNode is neither copyable nor movable.
   TdiNode(const TdiNode&) = delete;
@@ -90,7 +92,8 @@ class TdiNode {
           TdiPacketioManager* tdi_packetio_manager,
           TdiPreManager* tdi_pre_manager,
           TdiCounterManager* tdi_counter_manager,
-          TdiSdeInterface* tdi_sde_interface, int device_id);
+          TdiSdeInterface* tdi_sde_interface, int device_id,
+          bool initialized, uint64 node_id);
 
   // Write extern entries like ActionProfile, DirectCounter, PortMetadata
   ::util::Status WriteExternEntry(
